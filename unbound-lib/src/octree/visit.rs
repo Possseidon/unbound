d31798fe63f0a309023696e32d4468c7c08dfce3
Possseidon <xpossseidon@gmail.com>
2024-06-07 14:39:43 +0200

    Improve `OctreeVisitor` and `OctreeVisitorMut`

--- unbound-lib/src/octree/visit.rs
+++ unbound-lib/src/octree/visit.rs
@@ -1,5 +1,3 @@
-use crate::change_tracking::Mut;
-
 /// Allows for efficient traversal over an octree.
 pub trait OctreeVisitor {
     /// The type of the values stored in the octree.
@@ -7,12 +5,21 @@ pub trait OctreeVisitor {
     /// The type used to indicate specific regions within the octree.
     type Bounds;
 
-    /// Called for bounds that have one homogeneous value.
+    /// Called for homogeneous bounds.
     fn visit_bounds(&mut self, bounds: Self::Bounds, value: &Self::Value);
-    /// Should return `true` if the visitor should enter the split at the given bounds.
+    /// Called for heterogeneous bounds.
     ///
-    /// Likewise it should return `false` if the bounds are not of interest to the visitor.
-    fn visit_split(&mut self, bounds: Self::Bounds) -> bool;
+    /// Should return whether the visitor should skip or enter the split.
+    fn visit_split(&mut self, bounds: Self::Bounds) -> VisitSplit;
+}
+
+/// Describes how the visitation should continue when a split was encountered.
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+pub enum VisitSplit {
+    /// Skips this split.
+    Skip,
+    /// Enters this split, causing additional visit calls.
+    Enter,
 }
 
 /// Allows for efficient traversal and modification of an octree.
@@ -26,25 +33,36 @@ pub trait OctreeVisitorMut {
 
     /// Called for values that can no longer be split.
     ///
-    /// This prevents accidentally returning `true` from [`OctreeVisitorMut::visit_bounds_mut`] if
-    /// the bounds can no longer be split.
-    fn visit_value_mut(&mut self, pos: Self::Pos, value: &mut Mut<Self::Value>);
-    /// Called for bounds that have one homogeneous value.
+    /// If [`Some`] is returned, `value` is updated, otherwise `value` remains unchanged.
     ///
-    /// Should return `true` if there is no longer one unique value for these bounds, upon which the
-    /// bounds are split up.
-    fn visit_bounds_mut(&mut self, bounds: Self::Bounds, value: &mut Mut<Self::Value>) -> bool;
-    /// Should return whether the visitor should enter, skip or fill the split.
-    fn visit_split_mut(&mut self, bounds: Self::Bounds) -> VisitSplit<Self::Value>;
+    /// It is perfectly fine to return [`Some`] with a copy of the original `value`. The value will
+    /// be checked for changes internally as a safe-guard.
+    fn visit_value_mut(&mut self, pos: Self::Pos, value: &Self::Value) -> Option<Self::Value>;
+    /// Called for bounds.
+    ///
+    /// `value` is set if the bounds contain one homogeneous value.
+    ///
+    /// Should return whether the visitor should skip, fill or split the bounds.
+    ///
+    /// It is perfectly fine to return [`VisitBoundsMut::Fill`] with the same original `value`. The
+    /// value will be checked for changes internally as a safe-guard.
+    fn visit_bounds_mut(
+        &mut self,
+        bounds: Self::Bounds,
+        value: Option<&Self::Value>,
+    ) -> VisitBoundsMut<Self::Value>;
 }
 
-/// Describes how the visitation should continue when a split was encountered.
+/// Describes how the visitation should continue when homogeneous bounds were encountered.
+///
+/// Basically has the same structure as [`VisitSplitMut`] but with slightly different variant names
+/// to make their semantic meaning more accurate.
 #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
-pub enum VisitSplit<T> {
-    /// Skips this split.
+pub enum VisitBoundsMut<T> {
+    /// Skips these bounds without modifying it.
     Skip,
-    /// Enters this split, causing additional visit calls.
-    Enter,
-    /// Sets the split to one homogeneous value and does **not** enter it.
+    /// Fills the bounds with the value and then skips over it.
     Fill(T),
+    /// Splits these bounds up, causing additional visit calls.
+    Split,
 }
